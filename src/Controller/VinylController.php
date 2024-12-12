23d7f53cca4247582c997f5b66f4137c3042f01e
Dylane Chedjou <dechedjou@t3-it.de>
2024-12-12 14:22:33 +0100

    tuto copied

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+class VinylController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(){
+        
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
+       // return new Response('In the Beginning was the word...');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse (string $slug = null)
+    {
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
+    }
+    
+}
