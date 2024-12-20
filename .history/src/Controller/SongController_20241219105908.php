dc32ebc913bd6bc39843961beb3c5152e0a173a5
Dylane Chedjou <dechedjou@t3-it.de>
2024-12-20 13:48:03 +0100

    Alles funktioniert mit dem Musik

--- .history/src/Controller/SongController_20241219105908.php
+++ .history/src/Controller/SongController_20241219105908.php
@@ -6,13 +6,12 @@ namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}',methods:['GET'], name:'api_songs_get_one')]
+    #[Route('/api/songs/{id<\d+>}',methods:['GET'], name:'api songs get one')]
     public function getSong(int $id, LoggerInterface $logger) :Response{
         // TODO query the database
         $song = [
