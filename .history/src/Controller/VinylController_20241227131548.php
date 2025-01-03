294c31e09072f4d962500ad009423988a99834b6
Dylane Chedjou <dechedjou@t3-it.de>
2025-01-03 11:13:55 +0100

    neue Service MixRepo

--- .history/src/Controller/VinylController_20241227131548.php
+++ .history/src/Controller/VinylController_20241227131548.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
-use App\Service\MixRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 use function Symfony\Component\String\u;
 
@@ -31,10 +32,13 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse( MixRepository $mixRepository, string $slug = null): Response
+    public function browse(HttpClientInterface $httpClient,CacheInterface $cache, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
+        $mixes = $cache->get('mixes_data', function() use ($httpClient){
+            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+        return $response->toArray();
+        });
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
--- .history/src/Controller/VinylController_20250102143143.php
+++ .history/src/Controller/VinylController_20250102143143.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Service\MixRepository;
+use App\Serve\MixRepository;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
