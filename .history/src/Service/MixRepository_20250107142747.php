61334c9dda1e08ad211f18ecf61ecaff319f6516
Dylane Chedjou <dechedjou@t3-it.de>
2025-01-07 14:46:10 +0100

    scoped clietn created in framework.yaml

--- .history/src/Service/MixRepository_20250107142747.php
+++ .history/src/Service/MixRepository_20250107142747.php
@@ -11,7 +11,7 @@ class MixRepository
 {
 
     public function __construct(
-        private HttpClientInterface $githubContentClient,
+        private HttpClientInterface $httpClient,
         private CacheInterface $cache,
         #[Autowire('%kernel.debug%')]
         private bool $isDebug 
@@ -22,7 +22,7 @@ class MixRepository
     {
            return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem){
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->httpClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
         return $response->toArray();
         });
